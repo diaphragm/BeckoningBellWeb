8b7474cd35ba5248bf549e9d24bec29de7c8157a
diaphragm <>
2019-11-14 11:37:44 +0900

    create admin page

---
+++ app/admin/messages.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register Message do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :text, :user, :bell_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:text, :user, :bell_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  Message.column_names.each do |col|
+    filter col
+  end
+  filter :bell_id, as: :select
+end
