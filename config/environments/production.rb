8ed87cc4ec714ca52132e9835c96184f992c36a9
diaphragm <>
2019-11-17 22:57:24 +0900

    ドメインを反映

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://bloodborne.live-on.net' ]
+  config.action_cable.allowed_request_origins = [ 'https://bloodborne.live-on.net', 'https://bloodborne.9kv.org' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
