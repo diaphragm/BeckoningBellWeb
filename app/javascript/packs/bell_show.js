792b256931183f3fb2e00976a460e94e31e991a9
diaphragm <>
2019-11-17 19:59:57 +0900

    SCSSの読み込み方が間違っていたのを修正

--- app/javascript/packs/bell_show.js
+++ app/javascript/packs/bell_show.js
@@ -4,7 +4,6 @@ import Vue from 'vue/dist/vue.esm'
 import { csrfToken } from '@rails/ujs'
 
 import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 document.addEventListener('DOMContentLoaded', async () => {
