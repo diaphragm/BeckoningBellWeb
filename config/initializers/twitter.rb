9c7be6578aa715aa15a8dc3e74bb184c77e8fecd
diaphragm <>
2019-11-30 23:37:21 +0900

    Twitter投稿先を環境によって変えるようにした

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,15 @@
 module Twitter
   CLIENT = Twitter::REST::Client.new do |config|
-    config.consumer_key        = Rails.application.credentials.twitter[:consumer_key]
-    config.consumer_secret     = Rails.application.credentials.twitter[:consumer_secret]
-    config.access_token        = Rails.application.credentials.twitter[:access_token]
-    config.access_token_secret = Rails.application.credentials.twitter[:access_secret]
+    if Rails.env.production?
+      config.consumer_key        = Rails.application.credentials.twitter[:consumer_key]
+      config.consumer_secret     = Rails.application.credentials.twitter[:consumer_secret]
+      config.access_token        = Rails.application.credentials.twitter[:access_token]
+      config.access_token_secret = Rails.application.credentials.twitter[:access_secret]
+    else
+      config.consumer_key        = Rails.application.credentials.twitter_dev[:consumer_key]
+      config.consumer_secret     = Rails.application.credentials.twitter_dev[:consumer_secret]
+      config.access_token        = Rails.application.credentials.twitter_dev[:access_token]
+      config.access_token_secret = Rails.application.credentials.twitter_dev[:access_secret]
+    end
   end
 end
