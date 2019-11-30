19679e3def6e59d5890150c890a93cf56f8e2f43
diaphragm <>
2019-12-01 02:30:24 +0900

    Twitterへの投稿、更新、削除の方法をreply_toでツイートを追加していくように変更

--- app/controllers/maintenance_controller.rb
+++ app/controllers/maintenance_controller.rb
@@ -9,7 +9,10 @@ class MaintenanceController < ApplicationController
       if bell.delete_logical
         ActionCable.server.broadcast("room_#{bell.id}", {deleted: true})
         begin
-          Twitter::CLIENT.destroy_status(bell.tweet_uri)
+          # Twitter::CLIENT.destroy_status(bell.tweet_uri)
+          tweet = %[【終了】募集は終了しました。 ] + bell.tweet_uri.to_s
+          Twitter::CLIENT.update!(tweet, in_reply_to_status_id: bell.tweet_uri[/\d+$/])
+
           items << {err: false, data: bell}
         rescue
           items << {err: true, data: bell}
